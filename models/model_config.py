049783e83bec923e31cf7c156128f55c0b7e1e08
olshevskiyS <solshevskiy.87@gmail.com>
2025-03-24 23:07:40 +0300

    feat: add ppo config selection for scenarios, add optuna in requirements.txt

--- models/model_config.py
+++ models/model_config.py
@@ -1,4 +1,23 @@
-PPO_DEFAULT_CONFIG = {
+ppo_config = {
+"PPO_SCENARIO1_CONFIG":
+{
+    "policy": "MlpPolicy",
+    "verbose": 1,
+    "learning_rate": 0.0001,
+    "n_steps": 4096,
+    "batch_size": 256,
+    "n_epochs": 5,
+    "gamma": 0.99,
+    "gae_lambda": 0.95,
+    "clip_range": 0.2,
+    "clip_range_vf": 0.2,
+    "ent_coef": 0.01,
+    "total_timesteps": 10000,
+    "n_envs": 1,
+},
+
+"PPO_SCENARIO2_CONFIG":
+{
     "policy": "MlpPolicy",
     "verbose": 1,
     "learning_rate": 0.0001,
@@ -13,21 +32,6 @@ PPO_DEFAULT_CONFIG = {
     "total_timesteps": 10000,
     "n_envs": 1,
 }
-
-PPO_FINISH_EXTINGUISHING_CONFIG = {
-    "policy": "MlpPolicy",
-    "verbose": 1,
-    "learning_rate": 0.0001,
-    "n_steps": 4096,
-    "batch_size": 256,
-    "n_epochs": 5,
-    "gamma": 0.99,
-    "gae_lambda": 0.95,
-    "clip_range": 0.2,
-    "clip_range_vf": 0.2,
-    "ent_coef": 0.01,
-    "total_timesteps": 10000,
-    "n_envs": 1,
 }
 
 test_episodes = 10
--- models/train_model.py
+++ models/train_model.py
@@ -1,56 +1,65 @@
 import os
+import models.model_config as config
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import EvalCallback
-
 from envs.FireEnv import FireEnv
 from envs.FireEnv2 import FireEnv2
-from models.model_config import PPO_DEFAULT_CONFIG, PPO_FINISH_EXTINGUISHING_CONFIG
 from utils.logging_files import tensorboard_log_dir, model_name, best_model_path
 
 
 def train_and_evaluate(scenario, fire_count, obstacle_count):
-    cfg = PPO_DEFAULT_CONFIG
-    # cfg = PPO_FINISH_EXTINGUISHING_CONFIG
 
+    cfg = {}
     def make_env():
+        nonlocal cfg
         if scenario == 1:
-            env = FireEnv(fire_count=fire_count, obstacle_count=obstacle_count, render_mode=None)
+            env = FireEnv(fire_count = fire_count,
+                          obstacle_count = obstacle_count,
+                          render_mode = None)
+            cfg = config.ppo_config["PPO_SCENARIO1_CONFIG"]
         elif scenario == 2:
-            env = FireEnv2(fire_count=fire_count, obstacle_count=obstacle_count, render_mode=None)
+            env = FireEnv2(fire_count = fire_count,
+                           obstacle_count = obstacle_count,
+                           render_mode = None)
+            cfg = config.ppo_config["PPO_SCENARIO2_CONFIG"]
         else:
             raise ValueError(f"Неизвестный сценарий: {scenario}.\n"
                              f" Допустимые значения: 1 или 2.")
         return env
 
-    vec_env = make_vec_env(make_env, n_envs=cfg["n_envs"])
-    os.makedirs(tensorboard_log_dir, exist_ok=True)
+    train_env = make_env()
+    vec_env = make_vec_env(lambda: train_env, n_envs = cfg["n_envs"])
+    os.makedirs(tensorboard_log_dir, exist_ok = True)
+
     model = PPO(
-        policy=cfg["policy"],
-        env=vec_env,
-        verbose=cfg["verbose"],
-        learning_rate=cfg["learning_rate"],
-        n_steps=cfg["n_steps"],
-        batch_size=cfg["batch_size"],
-        n_epochs=cfg["n_epochs"],
-        gamma=cfg["gamma"],
-        gae_lambda=cfg["gae_lambda"],
-        clip_range=cfg["clip_range"],
-        clip_range_vf=cfg["clip_range_vf"],
-        ent_coef=cfg["ent_coef"],
-        tensorboard_log=tensorboard_log_dir
+        policy = cfg["policy"],
+        env = vec_env,
+        verbose = cfg["verbose"],
+        learning_rate = cfg["learning_rate"],
+        n_steps = cfg["n_steps"],
+        batch_size = cfg["batch_size"],
+        n_epochs = cfg["n_epochs"],
+        gamma = cfg["gamma"],
+        gae_lambda = cfg["gae_lambda"],
+        clip_range = cfg["clip_range"],
+        clip_range_vf = cfg["clip_range_vf"],
+        ent_coef = cfg["ent_coef"],
+        tensorboard_log = tensorboard_log_dir
     )
+
     eval_callback = EvalCallback(
         vec_env,
-        best_model_save_path=best_model_path,
-        log_path=tensorboard_log_dir,
-        eval_freq=1000,
-        render=False
+        best_model_save_path = best_model_path,
+        log_path = tensorboard_log_dir,
+        eval_freq = 1000,
+        render = False
     )
-    model.learn(total_timesteps=cfg["total_timesteps"], progress_bar=True)
-    mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes=10)
+
+    model.learn(total_timesteps = cfg["total_timesteps"], progress_bar = True)
+    mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes = 10)
     print(f"Среднее вознаграждение после тренировки: {mean_reward} +/- {std_reward}")
     model.save(model_name + str(scenario))
     return model
